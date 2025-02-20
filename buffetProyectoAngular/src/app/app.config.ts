e317d6337f1ec4147bfde5ff9c52c1375a12b2a3
ignacio mariano <you@example.com>
2025-02-20 13:21:17 -0300

    quita de repo anidado

--- buffetProyectoAngular/src/app/app.config.ts
+++ buffetProyectoAngular/src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+//h
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
